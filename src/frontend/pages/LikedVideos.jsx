4c89fc7c1fc22123a3a79c61b2f398d8937fec00
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2022-04-17 19:01:40 +0530

    [FEAT]: added video details page and other page components

--- src/frontend/pages/LikedVideos.jsx
+++ src/frontend/pages/LikedVideos.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { LikedVideoCard } from "../components";
+import { useLikedVideos } from "../context/providers";
 
 const LikedVideos = () => {
-  return <div>LikedVideos</div>;
+  const {
+    likedVideosState: { likedVideos },
+  } = useLikedVideos();
+
+  return (
+    <div className="likedVideoscontainer">
+      <div className="spacer-3rem"></div>
+      <div className="spacer-3rem"></div>
+      <div className="spacer-3rem"></div>
+
+      <div className="likedVideosmain">
+        {likedVideos &&
+          likedVideos.map((video) => (
+            <LikedVideoCard video={video} key={video._id} />
+          ))}
+      </div>
+
+      <div className="spacer-3rem"></div>
+      <div className="spacer-3rem"></div>
+      <div className="spacer-3rem"></div>
+    </div>
+  );
 };
 
 export default LikedVideos;
--- src/frontend/pages/WatchLater.jsx
+++ src/frontend/pages/WatchLater.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { LikedVideoCard } from "../components";
+import { useWatchLater } from "../context/providers";
 
 const WatchLater = () => {
-  return <div>WatchLater</div>;
+  const {
+    watchLaterState: { watchLater },
+  } = useWatchLater();
+
+  return (
+    <div className="likedVideoscontainer">
+      <div className="spacer-3rem"></div>
+      <div className="spacer-3rem"></div>
+      <div className="spacer-3rem"></div>
+
+      <div className="likedVideosmain">
+        {watchLater &&
+          watchLater.map((video) => (
+            <LikedVideoCard video={video} key={video._id} />
+          ))}
+      </div>
+
+      <div className="spacer-3rem"></div>
+      <div className="spacer-3rem"></div>
+      <div className="spacer-3rem"></div>
+    </div>
+  );
 };
 
 export default WatchLater;
