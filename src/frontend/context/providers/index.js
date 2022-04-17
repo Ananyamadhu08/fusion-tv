501702b67ebd4472940438dd2dee6b01fd9b45aa
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2022-04-17 17:52:22 +0530

    [FEAT]: added liked videos constants, provider and reducer

--- src/frontend/context/providers/index.js
+++ src/frontend/context/providers/index.js
@@ -4,3 +4,4 @@ export { VideosProvider, useVideos } from "./VideosProvider";
 export { FiltersProvider, useFilters } from "./FilterProvider";
 export { PlaylistProvider, usePlaylist } from "./PlaylistProvider";
 export { WatchLaterProvider, useWatchLater } from "./WatchLaterProvider";
+export { LikedVideosProvider, useLikedVideos } from "./LikedVideosProvider";
