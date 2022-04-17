c26b2d0a24bfc2437277d080da6f934cd0504d3d
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2022-04-17 18:50:55 +0530

    [FEAT]: added history utils and services

--- src/frontend/utils/index.js
+++ src/frontend/utils/index.js
@@ -18,3 +18,10 @@ export {
   addVideoToLikedVideos,
   removeVideoFromLikedVideos,
 } from "./likedVideosUtils";
+
+export {
+  getAllHistory,
+  deleteAllHistory,
+  addVideoToHistory,
+  removeVideoFromHistory,
+} from "./historyUtils.js";
