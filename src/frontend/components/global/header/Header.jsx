70ad2c276291de6336efaeaf0b2ad839ec0d44cc
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2022-05-29 14:14:04 +0530

    [FIX]: fixed header

--- src/frontend/components/global/header/Header.jsx
+++ src/frontend/components/global/header/Header.jsx
@@ -14,7 +14,8 @@ function Header() {
               className="flex align-items-center"
               style={{ gap: "0.5rem" }}
             >
-              <h5 className=" cursor-pointer mr-16 text-white  text-2xl text-italic">
+              <i class="fa-solid fa-video text-3xl text-white"></i>
+              <h5 className=" cursor-pointer mr-16 text-white  text-xl text-italic">
                 <span className="text-rose-500 text-4xl">Fusion</span>Tv
               </h5>
             </Link>
